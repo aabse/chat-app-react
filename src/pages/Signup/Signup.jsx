4a6c8d444867fc70e088c040a9df24ec987d7c80
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-11-13 22:30:57 -0300

    Added react-hook-form lib for validation and signup feature

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,33 +1,80 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import './Signup.css'
+import { signup } from '../../services/auth'
+import { UserContext } from '../../context/userContext'
 
 export default function Signup() {
   const navigate = useNavigate()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const { register, handleSubmit, formState: {errors, isValid}, watch } = useForm()
+  const user = useContext(UserContext)
 
-  const submitSignup = () => {
-    navigate('/home')
+  const formOptions = {
+    email: {
+      required: 'Email is required'
+    },
+    password: {
+      required: 'Password is required',
+      minLength: {
+        value: 6,
+        message: 'Password must have at least 6 characters'
+      }
+    },
+    passwordConfirm: {
+      required: 'Password Confirm is required',
+      validate: (value) => 
+        value === watch('password') || 'Passwords must match'
+    }
   }
 
+  const submitSignup = handleSubmit((data) => {
+    console.log(errors)
+    if (isValid) {
+      signup(data)
+        .then(res => {
+          console.log(res)
+          user.setEmail(data.email)
+          navigate('/home')
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  })
+
+  
   return (
     <section className='signup'>
       <form onSubmit={submitSignup}>
         <div className='form__inputs'>
           <div className='form__input'>
-            <input type="text" name="email" id="email" placeholder='Email' onChange={setEmail} value={email} />
+            <input type="email" className={errors.email? 'error-input':''} 
+              {...register('email', formOptions.email)} />
+            {
+              errors.email && <span className='error-message'>{errors.email.message}</span>
+            }
           </div>
           <div className='form__input'>
-            <input type="password" name="password" id="password" placeholder='Password' onChange={setPassword} value={password} />
+            <input type="password" className={errors.password && 'error-input'}
+              {...register('password', formOptions.password)} />
+            {
+              errors.password && <span className='error-message'>{errors.password.message}</span>
+            }
           </div>
           <div className='form__input'>
-            <input type="password" name="password-confirm" id="password-confirm" placeholder='Password Confirmation' onChange={setConfirmPassword} value={confirmPassword} />
+            <input type="password" className={errors.passwordConfirm && 'error-input'}
+              {...register('passwordConfirm', formOptions.passwordConfirm)} />
+            {
+              errors.passwordConfirm && <span className='error-message'>{errors.passwordConfirm.message}</span>
+            }
+
           </div>
         </div>
         <button type="submit">Signup</button>
       </form>
+      <div>
+      </div>
     </section>
   )
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -13,3 +13,19 @@ export const login = async ({email, password}) => {
     console.log(error)
   }
 }
+
+export const signup = async ({email,password}) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password}),
+      credentials: 'include'
+    })
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
