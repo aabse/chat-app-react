c9470f23825f8353b309db5cb2072e4d257a86c0
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-11-17 23:32:34 -0300

    Added time to message

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -16,8 +16,14 @@ export default function Chat({room, roomName}) {
       console.log('receving new message')
       console.log(msg)
       setListMessages(prevListMessages => [...prevListMessages, msg])
+      listMessages.forEach(msg => {
+        console.log(msg)
+        let t = timeMessage(msg.createdAt)
+        console.log(t)
+      })
     })
 
+        
     return () => {
       socket.off('chat message')
     }
@@ -41,6 +47,14 @@ export default function Chat({room, roomName}) {
       })
   }
 
+  const timeMessage = (dateStr) => {
+    let date = new Date(dateStr)
+    const offset = date?.getTimezoneOffset()
+    date = new Date(date.getTime() - (offset*60*1000))
+    let time = date.toISOString().split('T')[1]
+    return time.split(':').slice(0,2).join(':')
+  }
+
   return (
     <div className='wrapper-chat'>
     <section className="chat">
@@ -50,7 +64,7 @@ export default function Chat({room, roomName}) {
           {listMessages.map((msg,key) => (
             <li key={key} className={(msg.user === user.data.id)? 'send':'receive'}>
               <div>{msg.message}</div>
-              <div>{msg.name}</div>
+                <small className='time'>{timeMessage(msg.createdAt)}</small>
             </li>
           ))}
         </ul>
